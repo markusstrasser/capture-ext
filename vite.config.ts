9a3450a371ef1eed22144623c7d4da164ae6d853
Markus Strasser <strasser.ms@gmail.com>
2023-06-03 18:51:41 +0200

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,94 @@
+import { crx } from '@crxjs/vite-plugin'
+import vue from '@vitejs/plugin-vue'
+import { dirname, join, relative, resolve } from 'path'
+import AutoImport from 'unplugin-auto-import/vite'
+import IconsResolver from 'unplugin-icons/resolver'
+import Icons from 'unplugin-icons/vite'
+import Components from 'unplugin-vue-components/vite'
+import { defineConfig } from 'vite'
+import Pages from 'vite-plugin-pages'
+import manifest from './manifest.config'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      '~': resolve(join(__dirname, 'src')),
+      src: resolve(join(__dirname, 'src')),
+    },
+  },
+  plugins: [
+    crx({ manifest }),
+
+    vue(),
+
+    Pages({
+      dirs: [
+        {
+          dir: 'src/pages',
+          baseRoute: '',
+        },
+        {
+          dir: 'src/options/pages',
+          baseRoute: 'options',
+        },
+        {
+          dir: 'src/popup/pages',
+          baseRoute: 'popup',
+        },
+      ],
+    }),
+
+    AutoImport({
+      imports: [
+        'vue',
+        'vue-router',
+        'vue/macros',
+        { 'webext-bridge': ['sendMessage', 'onMessage'] },
+      ],
+      dts: 'src/auto-imports.d.ts',
+    }),
+
+    // https://github.com/antfu/unplugin-vue-components
+    Components({
+      dirs: ['src/components'],
+      // generate `components.d.ts` for ts support with Volar
+      dts: 'src/components.d.ts',
+      resolvers: [
+        // auto import icons
+        IconsResolver({
+          prefix: 'icon',
+          enabledCollections: ['mdi'],
+        }),
+      ],
+    }),
+
+    // https://github.com/antfu/unplugin-icons
+    Icons(),
+
+    // rewrite assets to use relative path
+    {
+      name: 'assets-rewrite',
+      enforce: 'post',
+      apply: 'build',
+      transformIndexHtml(html, { path }) {
+        return html.replace(
+          /"\/assets\//g,
+          `"${relative(dirname(path), '/assets')}/`
+        )
+      },
+    },
+  ],
+  server: {
+    port: 8888,
+    strictPort: true,
+    hmr: {
+      port: 8889,
+      overlay: false,
+    },
+  },
+  optimizeDeps: {
+    include: ['vue', '@vueuse/core'],
+    exclude: ['vue-demi'],
+  },
+})
